5eb898fb05a41be9c25597265908a88af5eef679
Fikri <jamesfikrii@gmail.com>
2019-05-09 15:17:24 +0700

    array 0f object

--- day10/obejct.js
+++ day10/obejct.js
@@ -1,3 +1,6 @@
+// ============= OBJECT LITERAL =================
+
+
 var orang = {
     namaDpn:'Andi',
     usia:50,
@@ -20,6 +23,7 @@ Andi.pekerjaan = 'PNS';
 console.log(Andi)
 
 
+// ============= BUILDING OBJECT WITH FUNCTION =================
 var Orang = function(a,b,c){
     this.nama = a;
     this.usia = b;
@@ -31,3 +35,34 @@ var orang2 = new Orang('Seto',40,'Politisi')
 var orang3 = new Orang('Andi',50,'Direktur')
 
 console.log(orang3)
+
+
+
+// ============= BUILDING OBJECT CLASS =================
+
+
+class Mobil {
+    constructor(a,b,c,d){
+        this.nama = a;
+        this.warna = b;
+        this.merk = c;
+        this.tahun =d
+    }
+}
+
+
+var mobil1 = new Mobil('CRV' , 'Hitam' , 'Honda' , 2018)
+var mobil2 = new Mobil('Vios' , 'Hitam' , 'Honda' , 2013)
+
+var arr = [mobil1,mobil2]
+var print = ''
+for(var i = 0 ; i < arr.length ; i++){
+    print += i+1 + '. ' + arr[i].nama + ' Warna ' + arr[i].warna + ' Merk ' + arr[i].merk + ' dibuat pada tahun ' + arr[i].tahun + '\n'
+}
+
+
+console.log(print)
+
+// 1. CRV Warna Hitam Merk Honda dibuat pada tahun 2018
+// 2. Vios Warna Hitam Merk Honda dibuat pada tahun 2013
+// Array Of Obj
